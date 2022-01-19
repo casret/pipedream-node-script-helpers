5612da15ed2828cc7389b78b5bace7cfffcd52a4
Giao Phan <casret@pobox.com>
2022-01-18 17:28:03 -0800

    PIPEDREAM_STEPS semantic change

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const fs = require("fs")
-const steps = JSON.parse(process.env.PIPEDREAM_STEPS || "null")
+let steps
+try {
+	steps = JSON.parse(fs.readFileSync(process.env.PIPEDREAM_STEPS))
+} catch {
+	//ignore
+}
 const $ = {
 	export(name, value) {
 		fs.appendFileSync(process.env.PIPEDREAM_EXPORTS, `${name}:json=${JSON.stringify(value)}\n`)
